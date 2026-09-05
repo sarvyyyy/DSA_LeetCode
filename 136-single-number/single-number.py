bd6c2dfbaea7843cc41f97244a56bcbbd573805a
sarvesh <sarvyyy1522@gmail.com>
2026-09-05 10:52:32 +0530

    Time: 3 ms (60.38%) | Memory: 21.1 MB (69.98%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,10 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        x = -1
-        for i in nums:
-            if x==-1:
-                x = i
-            elif i==x:
-                x=-1
+        x = 0
+        for num in nums:
+            x = x ^ num
         return x
